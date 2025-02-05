6c4197b8f8c67387eecb20b10843e22b3d514f2a
Dv-AlejandroEH <aespher121@gmail.com>
2025-02-05 12:43:00 +0100

    refactor e1 and make a2

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,8 +1,16 @@
+# def calcular_factorial(n):
+#     factorial = 1
+#     for i in range(n):
+#         factorial *= n-i
+#     return factorial
+# numero = int(input('Introduzca un número para calcular su factorial: '))
+# resultado = calcular_factorial(numero)
+# print(resultado)
+
 def calcular_factorial(n):
-    factorial = 1
-    for i in range(n):
-        factorial *= n-i
-    return factorial
+    if n == 0 or n == 1:
+        return n
+    return n * calcular_factorial(n-1)
 numero = int(input('Introduzca un número para calcular su factorial: '))
 resultado = calcular_factorial(numero)
 print(resultado)
---
+++ ejercicio2.py
@@ -0,0 +1,10 @@
+def calcular_enesimo(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return calcular_enesimo(n-1) + calcular_enesimo(n-2)
+numero = int(input('Introduzca un número para calcular el enésimo: '))
+resultado = calcular_enesimo(numero)
+print(resultado)
